2e963b33bd0334f37c55555ae1127020ae9ea03d
Nebyou <nebyou.4k@gmail.com>
2021-11-16 16:33:28 -0500

    Initial

---
+++ react-app/src/components/LoginPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LoginPage.css";
+
+export default function LoginPage() {
+  return (
+    <div className="login__page-container">
+      <div className="login--left-side">TEXT</div>
+      <div className="login--right-side">TEXT</div>
+    </div>
+  );
+}
