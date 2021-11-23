87fcab33b78fdc4419645b23d3c7be7d240469e6
Nebyou <nebyou.4k@gmail.com>
2021-11-23 12:33:14 -0500

    log

--- app/api/files.py
+++ app/api/files.py
@@ -4,8 +4,6 @@ from flask_login import login_required
 from app.config import Config
 from app.aws_s3 import upload_file_to_s3
 from app.models import db, User
-import boto3
-import botocore
 
 #any other imports as needed
 
@@ -32,11 +30,16 @@ def upload_user_data():
     file_callback = user.profile_pic if "file" not in request.files else request.files["file"]
     file_callback2 = user.banner_pic if "file2" not in request.files else request.files["file2"]
 
+    print(f"Inside POST route.")
+
     if file_fallback_1 == 1:
         file_url_accepted = upload_file_to_s3(file_callback, Config.S3_BUCKET)
+        print(f"\n\n\n{file_url_accepted}\n\n\n")
         user.profile_pic = file_url_accepted
     else:
+        print(f"\n\n\n{file_callback}\n\n\n")
         user.profile_pic = file_callback
+
 
     if file_fallback_2 == 1:
         file2_url_accepted = upload_file_to_s3(file_callback2, Config.S3_BUCKET)
